72ae008341664ee68bdfb586c47a57e5d10f1769
AndreiMurillos <carlitos.tuna@hotmail.com>
2022-09-26 20:18:21 -0500

    Se usaron funciones para activar cierto menu y asi los demas al clickearlos cuando uno este activo, desaparezca el que estaba activo y viceversa

--- main.js
+++ main.js
@@ -2,14 +2,30 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() 
+{
+    mobileMenu.classList.add('inactive');
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu(){
+function toggleMobileMenu()
+{
+    desktopMenu.classList.add('inactive');
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside()
+{
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
